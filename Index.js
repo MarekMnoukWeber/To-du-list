f45450740b0e30abc4cc56d852bfe370e107cd72
MarekMnoukWeber <marek@weber.cz>
2024-11-18 19:17:27 +0100

    Fungující označení

--- Index.js
+++ Index.js
@@ -1,30 +1,47 @@
 const stredni = document.querySelector(".stredni")
+const zadane_ukoly = document.getElementById("zadane")
+const splnene_ukoly = document.getElementById("splnene")
 
 let Prikozave_tlacitko = document.querySelector(".button")
 let Prikazovy_radek = document.querySelector("#task-to-add")
-let zadane_ukoly = document.querySelector(".text_zadane")
+let zadane = 0
+let splnene = 0
 
+function splnit_ukol(task) {
+    task.classList.toggle("hotovo") ? splnene++ : splnene--
+    splnene_ukoly.innerHTML = splnene
+}
 
+function smazat_ukol(smazany_ukol) {
+    smazany_ukol.remove()
+    zadane -= 1
+    zadane_ukoly.innerHTML = zadane
+}
 
 function Vytvoř_ukol(Název) {
     let novy_ukol = document.querySelector(".vzor").cloneNode(true)
 
     novy_ukol.querySelector("p").innerHTML = Název 
     novy_ukol.classList.remove("vzor")
-    new_task.querySelector(".zadane").onclick = e => {
-        e.target.classList.toggle("zadane")
-    }
+    novy_ukol.querySelector("#Oznacit_za_splnene").onclick = e => splnit_ukol(e.target.parentElement)
+    novy_ukol.querySelector("#Smazat").onclick = e => smazat_ukol(e.target.parentElement)
 
-    stredni.append(novy_ukol)
+    
+    return novy_ukol
 }
 
+function add_task (jmeno) {
+    let novy_ukol = Vytvoř_ukol(jmeno)
+    stredni.append(novy_ukol)
+    zadane_ukoly.innerHTML = ++zadane
+}
 
 Prikozave_tlacitko.onclick = e => {
     if (Prikazovy_radek.value == "") {
         alert("Neťapká to")
 
     } else {
-        Vytvoř_ukol(Prikazovy_radek.value)
+        add_task(Prikazovy_radek.value)
 
     }
 }
