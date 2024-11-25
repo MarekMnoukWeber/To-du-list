3f49091285fe7a33ab76bea4c2190ffddda47779
MarekMnoukWeber <marek@weber.cz>
2024-11-25 19:12:11 +0100

    hotavo

--- Index.js
+++ Index.js
@@ -4,18 +4,41 @@ const splnene_ukoly = document.getElementById("splnene")
 const Prikozave_tlacitko = document.querySelector(".button")
 const Prikazovy_radek = document.querySelector("#task-to-add")
 
-let zadane = 0
-let splnene = 0
+let zadane = localStorage.getItem("zadane_ukoly")
+let splnene = localStorage.getItem("splnene_ukoly")
+
+splnene = (splnene == null) ? 0 : splnene
+zadane = (zadane == null) ? 0 : zadane
+
+function načti_data() {
+    splnene_ukoly.innerHTML = splnene
+    zadane_ukoly.innerHTML = zadane
+    let ukoly = localStorage.getItem("ukoly")
+    if (ukoly != null) {
+        stredni.innerHTML = ukoly
+        for (const ukol of stredni.children ) {
+            ukol.querySelector("#Oznacit_za_splnene").onclick = e => splnit_ukol(e.target.parentElement)
+            ukol.querySelector("#Smazat").onclick = e => smazat_ukol(e.target.parentElement)
+        }
+    }
+}
+
+function ulož_ukol() {
+    localStorage.setItem("ukoly", stredni.innerHTML) 
+}
 
 function splnit_ukol(task) {
     task.classList.toggle("hotovo") ? splnene++ : splnene--
     splnene_ukoly.innerHTML = splnene
+    localStorage.setItem("splnene_ukoly", splnene)
+    ulož_ukol()
 }
 
 function smazat_ukol(smazany_ukol) {
     smazany_ukol.remove()
     zadane -= 1
     zadane_ukoly.innerHTML = zadane
+    ulož_ukol()
 }
 
 function Vytvoř_ukol(Název) {
@@ -23,7 +46,7 @@ function Vytvoř_ukol(Název) {
 
     novy_ukol.querySelector("p").innerHTML = Název 
     novy_ukol.classList.remove("vzor")
-    
+
     novy_ukol.querySelector("#Oznacit_za_splnene").onclick = e => splnit_ukol(e.target.parentElement)
     novy_ukol.querySelector("#Smazat").onclick = e => smazat_ukol(e.target.parentElement)
 
@@ -34,6 +57,8 @@ function dalsi_ukol (jmeno) {
     let novy_ukol = Vytvoř_ukol(jmeno)
     stredni.append(novy_ukol)
     zadane_ukoly.innerHTML = ++zadane
+    localStorage.setItem("zadane_ukoly", zadane)
+    ulož_ukol()
 }
 
 Prikozave_tlacitko.onclick = e => {
@@ -46,3 +71,4 @@ Prikozave_tlacitko.onclick = e => {
     }
 }
 
+načti_data()
